897dcd2c1fac10a4286f103fbcbe6035eb356d62
Pyanuk <144242312+Pyanuk@users.noreply.github.com>
2023-11-02 18:37:05 +0300

    Program.cs

--- Тортики/Program.cs
+++ Тортики/Program.cs
@@ -1,193 +1,273 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using Tortik;
 
-class Progra
+namespace Tortik
 {
-    static void Main(string[] args)
+    internal class ElementsofMenu
     {
-        bool exit = false;
-        List<CakeOrder> orders = new List<CakeOrder>();
 
-        while (!exit)
+        public ElementsofMenu(int el, string discription, int price, Action action)
         {
-            // Главное меню выбора торта
-            int cakeShape = ConsoleMenu.DisplayMenu("Выберите форму торта:", new string[] { "Круглый", "Квадратный", "Прямоугольный" });
-            int cakeSize = ConsoleMenu.DisplayMenu("Выберите размер торта:", new string[] { "Маленький", "Средний", "Большой" });
-            int cakeFlavor = ConsoleMenu.DisplayMenu("Выберите вкус торта:", new string[] { "Шоколадный", "Ванильный", "Фруктовый" });
-            int cakeQuantity = ConsoleMenu.DisplayMenu("Выберите количество тортов:", new string[] { "1", "2", "3" });
+            this.EL = el;
+            this.Discription = discription;
+            this.Price = price;
+            this.Action = action;
 
-            // Меню выбора глазури
-            int frostingType = ConsoleMenu.DisplayMenu("Выберите глазурь:", new string[] { "Шоколадная", "Сливочная", "Карамельная" });
+        }
 
-            // Меню выбора декора
-            int decorType = ConsoleMenu.DisplayMenu("Выберите декор:", new string[] { "Цветные конфетки", "Шоколадные стружки", "Фрукты" });
+        public int EL { get; set; }
+        public string Discription { get; set; }
+        public int Price { get; set; }
+        public Action Action { get; set; }
 
-            // Создание объекта заказа торта и добавление в список заказов
-            CakeOrder order = new CakeOrder((CakeShape)cakeShape, (CakeSize)cakeSize, (CakeFlavor)cakeFlavor, (CakeQuantity)cakeQuantity, (FrostingType)frostingType, (DecorType)decorType);
-            orders.Add(order);
 
-            Console.WriteLine("Заказ принят!");
+    }
+    public static class Global
+    {
+        public static int Price = 0;
+        public static List<string> Order = new List<string> { };
+    }
+}
+public class Menu
+    {
+        public Menu PlayMenu { get; set; }
+        public string Zagolovok { get; set; }
+        public string Arrow { get; set; }
+        public ConsoleColor ArrowColour { get; set; }
+        public ConsoleColor ForegroundColour { get; set; }
+        public ConsoleColor MenuItemColour { get; set; }
+        public ConsoleColor ZagolovokColour { get; set; }
 
-            // Подсчет суммарной цены и вывод информации о заказе
-            decimal totalCost = 0;
-            Console.WriteLine("Информация о заказе:");
-            foreach (CakeOrder cakeOrder in orders)
+        public int AllPrice { get; private set; } = 0;
+
+        private List<ElementsofMenu> ItemList;
+        public int[] prices = new int[] { };
+
+        private int position;
+        private bool Exit;
+        public int Cena;
+
+        public Menu(string arrow = "->")
+        {
+            ItemList = new List<ElementsofMenu>();
+            position = 0;
+
+            this.Arrow = arrow;
+            ArrowColour = ConsoleColor.Magenta;
+            ForegroundColour = ConsoleColor.White;
+            MenuItemColour = ConsoleColor.White;
+            ZagolovokColour = ConsoleColor.White;
+        }
+        public void Draw()
+        {
+            Console.Clear();
+            Console.WriteLine(Zagolovok);
+
+            for (int i = 0; i < ItemList.Count; i++)
             {
-                Console.WriteLine(cakeOrder.ToString());
-                totalCost += cakeOrder.GetTotalCost();
+                if (i == position)
+                {
+                    Console.ForegroundColor = ArrowColour;
+                    Console.Write(Arrow + " ");
+                    Console.WriteLine(ItemList[i].Discription);
+                    Console.ForegroundColor = ForegroundColour;
+                }
+                else
+                {
+                    Console.Write(new string(' ', (Arrow.Length + 0)));
+                    Console.WriteLine(ItemList[i].Discription);
+                }
             }
-            Console.WriteLine("Суммарная цена заказа: $" + totalCost);
+            Console.WriteLine("Цена:" + Global.Price);
+            Console.WriteLine("Заказ: ");
+            foreach (string item in Global.Order)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public void ShowMenu()
+        {
+            Console.CursorVisible = false;
+            Console.Clear();
+            Draw();
+            Exit = false;
+            while (!Exit)
+            {
+                MenuUpdate();
 
-            // Сохранение заказа в файл
-            SaveOrderToFile(orders);
+            }
+        }
 
-            // Проверка продолжения оформления заказа
-            Console.WriteLine("Желаете оформить еще один заказ? (Y/N)");
-            string option = Console.ReadLine();
-            exit = (option.ToLower() != "y");
+        public void HideMenu()
+        {
+
+            Exit = true;
+        }
+
+        public void MenuUpdate()
+        {
+            switch (Console.ReadKey().Key)
+            {
+                case ConsoleKey.Enter:
+                    {
+                        Console.Clear();
+                        Console.CursorVisible = true;
+                        ItemList[position].Action();
+                        Global.Price += ItemList[position].Price;
+                        Global.Order.Add(ItemList[position].Discription);
+                        Console.CursorVisible = false;
+                        Console.Clear();
+                        Draw();
+                        break;
+                    }
+                case ConsoleKey.Escape:
+                    {
+                        if (PlayMenu != null)
+                        {
+                            HideMenu();
+                        }
+                        break;
+                    }
+                case ConsoleKey.UpArrow:
+                    {
+                        position--;
+                        if (position < 0)
+                        {
+                            position++;
+                            Console.Clear();
+                            Draw();
+
+                        }
+                        break;
+                    }
+                case ConsoleKey.DownArrow:
+                    {
+
+                        if (position < (ItemList.Count - 1))
+                        {
+                            position++;
+                            Console.Clear();
+                            Draw();
+
+                        }
+                        break;
+                    }
+            }
+        }
+
+        public bool AddItem(int el, string discription, int price, Action action)
+        {
+            if (!ItemList.Any(item => item.EL == el))
+            {
+
+                ItemList.Add(new ElementsofMenu(el, discription, price, action));
+
+                return true;
+            }
+            return false;
         }
     }
-
-    static void SaveOrderToFile(List<CakeOrder> orders)
-    {
-        decimal totalCost = 0;
-        string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-        string filePath = Path.Combine(desktopPath, "Инфо_тортика.txt");
-
-        using (StreamWriter writer = new StreamWriter(filePath))
+internal class Program
+{
+        static void Main()
         {
-            writer.WriteLine("Информация о заказе:");
-            foreach (CakeOrder cakeOrder in orders)
-            {
-                writer.WriteLine(cakeOrder.ToString());
-            }
-            writer.WriteLine("Суммарная стоимость заказа: $" + totalCost);
+            Menu mainMenu = new Menu();
+            Menu FormaTorta = new Menu("~>");
+            FormaTorta.Zagolovok = "Форма торта";
+            FormaTorta.AddItem(0, "Квадрат - 400", 400, FormaTorta.HideMenu);
+            FormaTorta.AddItem(1, "Круг - 500", 500, FormaTorta.HideMenu);
+            FormaTorta.AddItem(2, "Сердечко - 700", 700, FormaTorta.HideMenu);
+            FormaTorta.AddItem(3, "Котик - 1000", 1000, FormaTorta.HideMenu);
+            FormaTorta.PlayMenu = mainMenu;
+
+
+
+            Menu SizeTorta = new Menu("~>");
+            SizeTorta.Zagolovok = "Размер тортика";
+            SizeTorta.AddItem(0, "Обычный(7-8 порций) - 1300", 1300, SizeTorta.HideMenu);
+            SizeTorta.AddItem(1, "Бенто-Тортик(1-2 порций) - 800", 800, SizeTorta.HideMenu);
+            SizeTorta.AddItem(2, "Средний(10-12 порций) - 1800", 1800, SizeTorta.HideMenu);
+            SizeTorta.AddItem(3, "Большой(18-20 порций) - 2500", 2500, SizeTorta.HideMenu);
+
+
+
+            SizeTorta.PlayMenu = mainMenu;
+
+            Menu TasteTorta = new Menu("~>");
+            TasteTorta.Zagolovok = "Вкус коржей";
+            TasteTorta.AddItem(0, "Шоколадные - 300", 300, TasteTorta.HideMenu);
+            TasteTorta.AddItem(1, "Ванильные - 350", 350, TasteTorta.HideMenu);
+            TasteTorta.AddItem(2, "Шоколадно-ванильеые - 500", 500, TasteTorta.HideMenu);
+            TasteTorta.AddItem(3, "Кокосовые - 400", 400, TasteTorta.HideMenu);
+            TasteTorta.prices = new int[] { 300, 350, 500, 400 };
+            TasteTorta.PlayMenu = mainMenu;
+
+            Menu InsideTorta = new Menu("~>");
+            InsideTorta.Zagolovok = "Начинка тортика";
+            InsideTorta.AddItem(0, "Шоколадная - 250", 250, InsideTorta.HideMenu);
+            InsideTorta.AddItem(1, "Клубничная - 300", 300, InsideTorta.HideMenu);
+            InsideTorta.AddItem(2, "Красный бархат - 400", 400, InsideTorta.HideMenu);
+            InsideTorta.AddItem(3, "Манго-маракуйя - 500", 500, InsideTorta.HideMenu);
+            InsideTorta.prices = new int[] { 250, 300, 400, 500 };
+
+
+            InsideTorta.PlayMenu = mainMenu;
+
+            Menu CountYarys = new Menu("~>");
+            CountYarys.Zagolovok = "Количество ярусов";
+            CountYarys.AddItem(0, "Один ярус - 500", 500, CountYarys.HideMenu);
+            CountYarys.AddItem(1, "Два яруса - 800", 800, CountYarys.HideMenu);
+            CountYarys.AddItem(2, "Три яруса - 1200", 1200, CountYarys.HideMenu);
+
+            CountYarys.PlayMenu = mainMenu;
+
+            Menu Dekor = new Menu("~>");
+            Dekor.Zagolovok = "Декор";
+            Dekor.AddItem(0, "Ягодный микс - 500", 500, Dekor.HideMenu);
+            Dekor.AddItem(1, "Фигурка из мастики - 300", 300, Dekor.HideMenu);
+            Dekor.AddItem(2, "Сладости - 400", 400, Dekor.HideMenu);
+            Dekor.AddItem(3, "Без декора", 0, Dekor.HideMenu);
+
+            Dekor.PlayMenu = mainMenu;
+
+            mainMenu.Zagolovok = "  Мяу-кафе с вкусными тортиками <3";
+            mainMenu.AddItem(0, "Форма Тортика", 0, FormaTorta.ShowMenu);
+            mainMenu.AddItem(1, "Размер Тортика", 0, SizeTorta.ShowMenu);
+            mainMenu.AddItem(2, "Вкус Коржей", 0, TasteTorta.ShowMenu);
+            mainMenu.AddItem(3, "Начинка тортика", 0, InsideTorta.ShowMenu);
+            mainMenu.AddItem(4, "Количество ярусов", 0, CountYarys.ShowMenu);
+            mainMenu.AddItem(5, "Дополнительный декор", 0, Dekor.ShowMenu);
+            mainMenu.AddItem(6, "Конец оформления заказа", 0, Exit);
+
+            mainMenu.ShowMenu();
+
         }
-    }
-}
 
-class ConsoleMenu
-{
-    public static int DisplayMenu(string headerText, string[] options)
+    private static void Exit()
     {
-        Console.Clear();
-        Console.WriteLine(headerText);
-        for (int i = 0; i < options.Length; i++)
+        Console.WriteLine("Ваш заказ оформлен!");
+
+        string path = "C:\\Users\\rhali\\Desktop\\История заказов.txt";
+        string text = "Описание заказа";
+        foreach (string item in Global.Order) { Console.WriteLine(item); };
+        string price = "Цена: " + Global.Price;
+        if (File.Exists(path))
         {
-            Console.WriteLine($"{i + 1}. {options[i]}");
+
+            File.AppendAllText(path, text);
+            File.AppendAllText(path, price);
         }
-        Console.WriteLine("Выберите номер пункта меню и нажмите Enter:");
-
-        ConsoleKeyInfo key;
-        int selectedIndex = 0;
-
-        do
+        else
         {
-            key = Console.ReadKey();
-
-            if (key.Key == ConsoleKey.UpArrow)
-            {
-                selectedIndex--;
-                if (selectedIndex < 0)
-                    selectedIndex = options.Length - 1;
-            }
-            else if (key.Key == ConsoleKey.DownArrow)
-            {
-                selectedIndex++;
-                if (selectedIndex >= options.Length)
-                    selectedIndex = 0;
-            }
-        } while (key.Key != ConsoleKey.Enter);
-
-        Console.WriteLine();
-
-        return selectedIndex;
-    }
-}
-
-enum CakeShape
-{
-    Круглый,
-    Квадратный,
-    Прямоугольный
-}
-
-enum CakeSize
-{
-    Маленький,
-    Средний,
-    Большой
-}
-
-enum CakeFlavor
-{
-    Шоколадный,
-    Ванильный,
-    Фруктовый
-}
-
-enum CakeQuantity
-{
-    Один = 1,
-    Два,
-    Три
-}
-
-enum FrostingType
-{
-    Шоколадная,
-    Сливочная,
-    Карамельная
-}
-
-enum DecorType
-{
-    Цветные_конфетки,
-    Шоколадные_стружки,
-    Фрукты
-}
-
-class CakeOrder
-{
-    private CakeShape shape;
-    private CakeSize size;
-    private CakeFlavor flavor;
-    private CakeQuantity quantity;
-    private FrostingType frostingType;
-    private DecorType decorType;
-
-    public CakeOrder(CakeShape shape, CakeSize size, CakeFlavor flavor, CakeQuantity quantity, FrostingType frostingType, DecorType decorType)
-    {
-        this.shape = shape;
-        this.size = size;
-        this.flavor = flavor;
-        this.quantity = quantity;
-        this.frostingType = frostingType;
-        this.decorType = decorType;
+            File.Create(path);
+        }
+        Environment.Exit(0);
     }
 
-    public decimal GetTotalCost()
-    {
-        decimal basePrice = 10;
-        decimal pricePerCake = 5;
-        decimal pricePerFrosting = 2;
-        decimal pricePerDecor = 3;
-
-        decimal totalCost = basePrice + ((int)quantity - 1) * pricePerCake + pricePerFrosting + pricePerDecor;
-
-        return totalCost;
-    }
-
-    public override string ToString()
-    {
-        return $"Торт: {shape}, {size}, {flavor}, Количество: {quantity}, Глазурь: {frostingType}, Декор: {decorType}";
-    }
-
-
 }
-
-
-
-
-
